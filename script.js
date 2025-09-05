2614f69b0707399b8a0c61aa01af1386d760d4f1
Stephanie <stephaniecharles147@gmail.com>
2025-09-05 19:59:10 +0100

    my assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = 8
+let num2 = 4
+let name = "Stephanie"
+
+ console.log(num1 + num2)
+ console.log(num1 - num2)
+ console.log(num1 * num2)
+ console.log(num1 / num2)
+ console.log("Hello " + name + ", the result of num1 + num2 is " + (num1 + num2));
